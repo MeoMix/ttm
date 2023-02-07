4738104e0a05b239c68be0d1a50ac8585b4add91
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2023-02-07 21:54:35 +0000

    WIP wasm support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_system(hello_world).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
 }
 
 fn hello_world() {
     println!("hello world!");
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
